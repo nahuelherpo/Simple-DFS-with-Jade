0858095c6b1e20be49ccecc83e27c6a1d4f34c31
nahuelherpo <nahuelherpo@gmail.com>
2022-11-15 13:51:00 -0300

    add lib

--- dfs.sh
+++ dfs.sh
@@ -10,16 +10,15 @@ else
 		echo "Error bad arguments"
 		exit 0
 	fi
-	exit 0
 fi
 echo "Compiling"
 javac -classpath lib/jade.jar -d classes myexamples/AgenteMovil.java
-echo "Up the main container"
-java -cp lib/jade.jar:classes jade.Boot -gui &
-sleep 5
-echo "Upping the others container"
-for agents in 0 1 2; do
-	java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost &
-	sleep 2
-done;
-java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost -agents "myAgent:AgenteMovil(${OPERATION})"
+#echo "Up the main container"
+#java -cp lib/jade.jar:classes jade.Boot -gui &
+#sleep 5
+#echo "Upping the others container"
+#for agents in 0 1 2; do
+#	java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost &
+#	sleep 2
+#done;
+#java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost -agents "myAgent:AgenteMovil(${OPERATION})"
