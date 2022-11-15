35790c5cf8da380ecc2508e9cb028560e26c957f
nahuelherpo <nahuelherpo@gmail.com>
2022-11-15 12:24:36 -0300

    add script

---
+++ dfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -eq 0 ] || [ $# -eq 1 ]; then
+	echo "You must indicate the operation on the DFS and the file path"
+	exit 0
+else
+	if [ $1 = "-r" ] || [ $1 = "-w" ]; then
+		OPERATION=$1
+		echo ${OPERATION}
+	else
+		echo "Error bad arguments"
+		exit 0
+	fi
+	exit 0
+fi
+echo "Compiling"
+javac -classpath lib/jade.jar -d classes myexamples/AgenteMovil.java
+echo "Up the main container"
+java -cp lib/jade.jar:classes jade.Boot -gui &
+sleep 5
+echo "Upping the others container"
+for agents in 0 1 2; do
+	java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost &
+	sleep 2
+done;
+java -cp lib/jade.jar:classes jade.Boot -gui -container -host localhost -agents "myAgent:AgenteMovil(${OPERATION})"
